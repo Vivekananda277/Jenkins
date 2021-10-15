3395b1447f7959b2b495d3f426480a33fca2e863
Vivek <vivekanandareddy7677@gmail.com>
2021-10-15 15:57:54 +0530

    All

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,23 +1,4 @@
-pipeline {
-    agent any
+@library('roboshop')_
 
-    environment {
-        SAMPLE_URL="www.google.com"
-        SLACK_TOKEN=credentials('slack')
-           }
-        stages {
-            stage('one') {
-                steps {
-                    sh 'echo Hello World'
-                    sh 'echo ${SAMPLE_URL}'
-                    sh 'echo ${SLACK_TOKEN}'
-                }
-            }
-            stage('two') {
-                steps {
-                        slackSend channel: '#nothing', message: 'Hello'
-                        slackSend channel: '#nothing', message: 'Nothing'
-                }
-            }
-        }
-    }
+log.info 'Starting'
+log.warning 'Nothing to do'
---
+++ seed.groovy
@@ -0,0 +1,24 @@
+folder('CI-Pipelines') {
+  displayName('CI-Pipelines')
+  description('CI-Pipelines')
+}
+pipelineJob('CI-Pipelines/frontend') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps58/_git/frontend')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
