bd2a6baab860402a4985d316974b7ff356934d19
Vivek <vivekanandareddy7677@gmail.com>
2021-10-18 08:31:38 +0530

    All

--- seed.groovy
+++ seed.groovy
@@ -2,23 +2,36 @@ folder('CI-Pipelines') {
   displayName('CI-Pipelines')
   description('CI-Pipelines')
 }
-pipelineJob('CI-Pipelines/frontend') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps58/_git/frontend')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
+
+def component= ["cart", "catalogue", "user", "shipping", "frontend", "payment"]
+
+def count = (component.size() -1 )
+
+ for(int i in 0..count) {
+   def j = component[i]
+   pipelineJob("CI-Pipelines/${j}") {
+     configure { flowdefinition ->
+       flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+         'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+           'userRemoteConfigs' {
+             'hudson.plugins.git.UserRemoteConfig' {
+               'url'("https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps58/_git/${j}")
+               'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+             }
+           }
+           'branches' {
+             'hudson.plugins.git.BranchSpec' {
+               'name'('*/tags/*')
+             }
+             'hudson.plugins.git.BranchSpec' {
+               'name'('*/main')
+             }
+           }
+         }
+         'scriptPath'('Jenkinsfile')
+         'lightweight'(true)
+       }
+     }
+   }
+ }
+
