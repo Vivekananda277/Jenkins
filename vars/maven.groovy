b6f0a12eeb268da816d530c50e94828b1949ee82
Vivek <vivekanandareddy7677@gmail.com>
2021-10-18 14:51:27 +0530

    Nothing

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -18,7 +18,7 @@ def call(String COMPONENT){
       stage('Check Code Quality'){
         steps {
           sh """
-            sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.31.141:9000 -Dsonar.login=${SONAR_TOKEN}
+            sonar-scanner -Dsonar.java.binaries=target/. -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.31.141:9000 -Dsonar.login=${SONAR_TOKEN}
            """
         }
       }
