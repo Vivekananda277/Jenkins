4401939acb7aeae7bd39ea7d61fff03ed4412b8b
Vivek <vivekanandareddy7677@gmail.com>
2021-10-18 13:04:36 +0530

    All

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -10,7 +10,7 @@ def call(String COMPONENT){
       stage('Check Code Quality'){
         steps {
           sh """
-             sonar-scanner -Dsonar.projectKey=sample -Dsonar.sources=. -Dsonar.host.url=http://172.31.31.141:9000 -Dsonar.login=17cd0356e039ed0f6fc3eeb48f1413b22461aba5
+             sonar-scanner -Dsonar.projectKey=sample -Dsonar.sources=. -Dsonar.host.url=http://172.31.31.141:9000                -Dsonar.login=17cd0356e039ed0f6fc3eeb48f1413b22461aba5
          """
         }
       }
