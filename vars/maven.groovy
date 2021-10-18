962eb6566edae6e36a149b708946d3922e7adba5
Vivek <vivekanandareddy7677@gmail.com>
2021-10-18 13:49:37 +0530

    Nothing

---
+++ vars/maven.groovy
@@ -0,0 +1,56 @@
+def call(String COMPONENT){
+  pipeline{
+    agent {
+      node {
+        label "JAVA"
+      }
+    }
+    environment{
+      SONAR_TOKEN=credentials('SONAR_TOKEN')
+    }
+    stages{
+      stage('Code Compile'){
+        steps{
+          sh 'mvn compile'
+        }
+      }
+
+      stage('Check Code Quality'){
+        steps {
+          sh """
+            sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.31.141:9000 -Dsonar.login=${SONAR_TOKEN}
+           """
+        }
+      }
+      stage('Lint checks'){
+        steps {
+          echo 'Lint checks'
+        }
+      }
+      stage('Unit Tests'){
+        steps {
+          echo 'Unit Tests'
+        }
+      }
+      stage('Prepare Artifacts'){
+        steps {
+          sh '''
+        cd static
+        zip -r ${COMPONENT}.zip *
+        '''
+        }
+      }
+      stage('Publish Artifacts'){
+        steps {
+          echo 'Publish Artifacts'
+        }
+      }
+    }
+
+    post {
+      always {
+        cleanWs()
+      }
+    }
+  }
+}
