ebfce383dc835d03b5cd2a1cab67b170a5e261ed
Vivek <vivekanandareddy7677@gmail.com>
2021-10-21 02:55:33 +0530

    Nothing

--- seed.groovy
+++ seed.groovy
@@ -3,35 +3,34 @@ folder('CI-Pipelines') {
   description('CI-Pipelines')
 }
 
-def component= ["cart", "catalogue", "user", "shipping", "frontend", "payment"]
+/// Running a loop to lkeep the code dry.
+def component = ["cart", "catalogue","user","payment","shipping","frontend"];
 
-def count = (component.size() -1 )
-
- for(int i in 0..count) {
-   def j = component[i]
-   pipelineJob("CI-Pipelines/${j}") {
-     configure { flowdefinition ->
-       flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
-         'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
-           'userRemoteConfigs' {
-             'hudson.plugins.git.UserRemoteConfig' {
-               'url'("https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps58/_git/${j}")
-               'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-             }
-           }
-           'branches' {
-             'hudson.plugins.git.BranchSpec' {
-               'name'('*/tags/*')
-             }
-             'hudson.plugins.git.BranchSpec' {
-               'name'('*/main')
-             }
-           }
-         }
-         'scriptPath'('Jenkinsfile')
-         'lightweight'(true)
-       }
-     }
-   }
- }
-
+def count=(component.size()-1)
+for (i in 0..count) {
+  def j=component[i]
+  pipelineJob("CI-Pipelines/${j}") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/'+j)
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/tags/*')
+            }
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
+}
